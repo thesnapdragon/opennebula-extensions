f1b8a3a2435d5bf1154cf2ff6cf159bf34b3a473
thesnapdragon <u.milan@gmail.com>
2013-06-19 09:18:45 +0200

    install script

--- opennebula_ssp/install.sh
+++ opennebula_ssp/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # install scripts for SimpleSAMLphp OpenNebula Connector
+# Milán Unicsovics, u.milan at gmail dot com
+# usage: ./install.sh
 
 sudo cp -r etc/* /etc/one/
 sudo cp -r lib/ruby/* /usr/lib/one/ruby/
