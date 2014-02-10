09d4fd4db6c678592fd4a58e3e27874dc8e3bd15
thesnapdragon <u.milan@gmail.com>
2014-02-10 13:52:10 +0100

    4.4_shib patch added: user + primary group creation

---
+++ opennebula_ssp/4.4_shibboleth/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# install scripts for SimpleSAMLphp OpenNebula Connector
+# Milán Unicsovics, u.milan at gmail dot com
+# usage: ./install.sh
+
+sudo cp -r etc/* /etc/one/
+sudo cp -r lib/ruby/* /usr/lib/one/ruby/
+sudo cp -r lib/sunstone/* /usr/lib/one/sunstone/
